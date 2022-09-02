4050dc2fc5df6be92c2d3e2236cf26949e622f82
pont-os <pontos@mm.st>
2022-09-02 20:53:21 -0300

    add story for primary button on focus

--- src/components/Button.stories.jsx
+++ src/components/Button.stories.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Button } from "./Button";
+import { within } from "@storybook/testing-library";
 
 export default {
   title: "Button",
@@ -10,3 +11,10 @@ const Template = (args) => <Button {...args} />;
 
 export const Primary = Template.bind({});
 Primary.args = { label: "Button" };
+
+export const PrimaryWithFocusState = Template.bind({});
+PrimaryWithFocusState.args = { label: "Button" };
+PrimaryWithFocusState.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement);
+  await canvas.getByRole("button").focus();
+};
